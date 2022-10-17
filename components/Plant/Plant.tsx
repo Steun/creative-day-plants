c349c5401e7cbc155313c5961524ad2248d0c796
Teun Kelting <teun@orbiter.digital>
2022-10-17 15:17:18 +0200

    fix: changes

--- components/Plant/Plant.tsx
+++ components/Plant/Plant.tsx
@@ -1,48 +1,65 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import * as THREE from 'three'
-import React, { useRef, useState } from 'react'
+import React, { Suspense, useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 
 import { EffectComposer, DepthOfField, Bloom, Noise } from '@react-three/postprocessing'
+import { ContactShadows, PresentationControls } from '@react-three/drei'
+import { useSpring, animated, config } from '@react-spring/three'
 
 import Testplant from './Testplant'
 
 export const Plant: React.FC = () => {
-  function Box(props: JSX.IntrinsicElements['mesh']) {
-    const ref = useRef<THREE.Mesh>(null!)
 
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-
-    useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-    
+  function renderPlant() {
+    const PlantModel = () => <Testplant castShadow position={[0, -2, 0]} />
     return (
-      <mesh
-        {...props}
-        ref={ref}
-        scale={clicked ? 2.5 : 1}
-        onClick={(event) => click(!clicked)}
-        onPointerOver={(event) => hover(true)}
-        onPointerOut={(event) => hover(false)}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
+      <PlantModel />
     )
   }
-
+  
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      {/* <Box position={[-1.2, 0, 0]} />
-      <Box position={[2.2, 0, 0]} /> */}
+    <Canvas 
+      camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 3, 7] }} 
+      dpr={[1, 2]}
+      flat={true}
+    >
+          <PresentationControls
+            global={true}
+            snap={false}
+            speed={1}
+            zoom={1} 
+            rotation={[0, 0, 0]}
+            polar={[0, 0]}
+            azimuth={[-Infinity, Infinity]}
+            config={{ mass: 0.5, tension: 50, friction: 16}}
+          >
+            <Suspense fallback={<span>Loading...</span>}>
 
-      <Testplant></Testplant>
+            {renderPlant()}
+            {/* <Bigtree scale={[0.5, 0.5, 0.5]} position={[-5, -1, -5]} castShadow /> */}
+            
+            <ContactShadows
+              frames={10}
+              position={[0, -1.05, 0]}
+              scale={10}
+              blur={10}
+              far={1.2}
+            />
+            
+            </Suspense>
+          
 
-      <EffectComposer>
-        <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={1} height={480} />
-        <Bloom luminanceThreshold={0.1} luminanceSmoothing={0.9} height={500} />
-        <Noise opacity={0.2} premultiply />
-      </EffectComposer>
+
+          </PresentationControls>
+        <EffectComposer>
+          {/* <Bloom luminanceThreshold={2} luminanceSmoothing={1.9} height={600} /> */}
+          <Noise opacity={0.1} premultiply />
+        </EffectComposer>
+
+        <ambientLight />
+        <pointLight position={[10, 30, 10]} intensity={0.5} />
+        <pointLight position={[32, 16, 10]} color={'#e4a227'}  intensity={0.8} />
     </Canvas>
   )
 }
